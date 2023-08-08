63b221a3613ecef80745980bedad63fa81c3e5c4
Tauc2005 <tauc2005@gmail.com>
2023-08-08 22:22:12 +0300

    Исправлен скрипт для работы с пользовательским полем - заголовок модального окна

--- scenes/gui/elemets/modal_wnd/modal_wnd.gd
+++ scenes/gui/elemets/modal_wnd/modal_wnd.gd
@@ -3,27 +3,22 @@ class_name ModalWindow
 
 signal on_window_closed()
 
-@export var Caption:String = "Заголовок" #_set set_caption
-@export var ShowExitButton:bool = true
-@export var ShowBackGround:bool = true
+@export var Caption:String = "Заголовок" : #_set set_caption
+	get: 
+		return Caption
+	set(value): 
+		Caption= value
+		$MarginContainer/NameWnd/Box/CaptionLabel.set("text",Caption)
+		
+@export var ShowExitButton:bool = true:
+	get: 
+		return $MarginContainer/BtnClose.visible
+	set(value):
+		$MarginContainer/BtnClose.visible = value
 
-@onready var ui_caption = get_node("MarginContainer/NameWnd/Box/CaptionLabel")
+@export var ShowBackGround:bool = true
 
 func _on_window_closed():
 	print ("(settings) _on_window_closed ")
 	emit_signal("on_window_closed")
-	pass # Replace with function body.
-
-
-func set_caption():
-	if not ui_caption == null:
-		ui_caption.set("text", Caption)
-	pass
-
-func update():
-	set_caption()
-	
-
-func _ready():
-	update()
 	pass # Replace with function body.
